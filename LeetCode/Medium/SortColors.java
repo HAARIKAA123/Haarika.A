1b0e0d0a663092a6b5412d7905d186ae39507018
HAARIKAA123 <161359765+HAARIKAA123@users.noreply.github.com>
2025-06-10 14:04:31 +0530

    Create SortColors.java

---
+++ LeetCode/Medium/SortColors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int temp;
+        int flag=0;
+        for(int i=0;i<nums.length-1;i++){
+            flag=0;
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                break;
+            }
+    
+        }
+    }
+}
